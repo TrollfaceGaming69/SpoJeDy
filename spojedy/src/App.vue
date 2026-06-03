6d1bb5a928e0733bd3b3f05cdfde2ab0805fc954
TrollfaceGemink <alfatihmiftahul.mz@gmail.com>
2026-06-03 20:20:05 +0700

    minor update

--- spojedy/src/App.vue
+++ spojedy/src/App.vue
@@ -1,21 +1,8 @@
 <script setup>
-import { useRouter } from 'vue-router'
-import Navbar from '@/component/navbar.vue';
-import SideBar from '@/component/sideBar.vue';
-
-const router = useRouter()
 </script>
 
 <template>
-      <div class="h-screen bg-black">
-        <Navbar/>
-        <div class="h-[90%] flex">
-            <SideBar/>
-            <router-view />
-        </div>
-        
-        <audio src="" preload="auto"></audio>
-    </div>
+  <router-view />
 </template>
 
 
